f0a346c230c5c9a54fa282f81d1cd0b37f2a0b20
Advanced-MRI-Lab-SJTU <83933600+Advanced-MRI-Lab-SJTU@users.noreply.github.com>
2022-04-02 14:41:20 +0800

    Delete utils directory

--- utils/fftnc.m
+++
@@ -1,8 +0,0 @@
-function im = fftnc(d)
-% im = fftnc(d)
-%
-% fftnc perfomrs a centered fftn
-%
-im = fftshift(fftn(fftshift(d)));
-end
-
--- utils/ifftnc.m
+++
@@ -1,7 +0,0 @@
-function im = ifftnc(d)
-% im = ifftnc(d)
-%
-% ifftnc perfomrs a centered ifftn
-%
-im = fftshift(ifftn(fftshift(d)));
-end
